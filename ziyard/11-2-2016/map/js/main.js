75e7d582d31131c9a151a2a319e57913486dfa97
Chamara Harindra <swatharindra@gmail.com>
2016-11-14 20:03:28 +0530

    Feature_add loading screen
    add loading screen when click on tabs

--- ziyard/11-2-2016/map/js/main.js
+++ ziyard/11-2-2016/map/js/main.js
@@ -14,19 +14,27 @@ $(document).ready(function(){
   // Tab switching
 	$('a.tabs').click(function(){
 		var tab_id = $(this).attr('data-tab');
+    //show the loading screen
+		$('div.loader').show();
+    setTimeout(function() { $("div.loader").hide(); }, 1000);
 
 		$('a.tabs').removeClass('current');
 		$('.tab-content').removeClass('current');
 
 		$(this).addClass('current');
 		$("#"+tab_id).addClass('current');
+
 	})
 
   // Result pane slide down
 	$( "#nearby" ).click(function() {
 	  event.stopPropagation();
+
 	  $( ".results-address" ).slideToggle("slow");
 		$( ".results-address" ).toggleClass("hidden");
+		//show the loading screen
+		$('div.loader').show();
+    setTimeout(function() { $("div.loader").hide(); }, 1000);
 	});
 
   // Show popover once hover on the result list
