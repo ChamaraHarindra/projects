6c7a3c4551fc956ac62f06c2a1b6533ae89b3e0c
Chamara Harindra <swatharindra@gmail.com>
2016-11-18 17:09:43 +0530

    Version-2 touch ups

--- ziyard/11-2-2016/map/js/main-v2.js
+++ ziyard/11-2-2016/map/js/main-v2.js
@@ -1,62 +1,56 @@
+$(document).ready(function () {
 
-$(document).ready(function(){
-
-  // Tab switching
-	$('a.tabs').click(function(){
+	// Tab switching
+	$('a.tabs').click(function () {
 		var tab_id = $(this).attr('data-tab');
 
 		$('a.tabs').removeClass('current');
 		$('.tab-content').removeClass('current');
 
 		$(this).addClass('current');
-		$("#"+tab_id).addClass('current');
+		$("#" + tab_id).addClass('current');
 
 		$('.results').hide();
 
 		$('div.loader').show();
-    setTimeout(function() { $("div.loader").hide(); }, 1000);
+		setTimeout(function () {
+			$("div.loader").hide();
+		}, 1000);
 
 	});
+	
+	
 
 	//  Grid to list
-    $('#list').click(function(event)
-	{
+	$('#list').click(function (event) {
 		event.preventDefault();
 		$('.grid-seperate').addClass('change-to-list');
 		$('.result-grid button.green').addClass('small');
 	});
 
-    $('#grid').click(function(event)
-	{
+	$('#grid').click(function (event) {
 		event.preventDefault();
 		$('.grid-seperate').removeClass('change-to-list');
 		$('.result-grid button.green').removeClass('small');
 	});
 
-	// Result pane slide down
-/*	$( "#search-location" ).click(function() {
-	  event.stopPropagation();
-
-	  $( ".results" ).slideToggle("slow");
-		$( ".results" ).toggleClass("hidden");
-		//show the loading screen
-		$('div.loader').show();
-    setTimeout(function() { $("div.loader").hide(); }, 1000);
-	});*/
-
-
 });
 
 
 //100% height on the search results
-$(function(){
-    $( "#my-location" ).click(function(){
-			  var h = $('.tab-data').offset()
-        $('.scroll-box-inner').css('max-height', ($(window).height() - h.top) + 'px');
-    });
-//100% height on the map
-		$(document).ready(function(){
-			  var h = $('.map').offset()
-        $('.map').css('max-height', ($(window).height() - h.top) + 'px');
-    });
+$(function () {
+	$("#my-location").click(function () {
+		var h = $('.tab-data').offset();
+		$('.scroll-box-inner').css('max-height', ($(window).height() - h.top) + 'px');
+	});
+	
+	$("#search-location").click(function () {
+		var h = $('.tab-data').offset();
+		$('.scroll-box-inner').css('max-height', ($(window).height() - h.top) + 'px');
+	});
+	//100% height on the map
+	$(document).ready(function () {
+		var h = $('.map').offset();
+		$('.map').css('max-height', ($(window).height() - h.top) + 'px');
+	});
 });
